4b2b3bd07babf3e42e22a7ba55b66878efadb50b
usman <usmankhan.hse@gmail.com>
2026-07-10 09:30:45 +0400

    declare constant roles.ts and User model in User.ts

---
+++ src/constants/roles.ts
@@ -0,0 +1,5 @@
+export enum UserRole{
+    ADMIN = "ADMIN",
+    DRIVER = "DRIVER",
+    CUSTOMER = "CUSTOMER"
+}
---
+++ src/models/User.ts
@@ -0,0 +1,53 @@
+import { UserRole } from "@/constants/roles";
+import mongoose, { model, models, Schema } from "mongoose";
+
+export type UserDocument = {
+    _id : mongoose.Types.ObjectId;
+    name: string;
+  email: string;
+  password: string;
+  phone?: string;
+  role: UserRole;
+  isVerified: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+const userSchema = new Schema<UserDocument>(
+    {
+        name: {
+         type: String,
+         required: true,
+        trim: true,
+        },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      trim: true,
+    },
+    role :{
+        type : String,
+        enum : Object.values(UserRole),
+        default: UserRole.CUSTOMER,
+        required: true
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+
+    }, {timestamps:true}
+);
+const User = models.User || model<UserDocument>("User", userSchema)
+export default User;
